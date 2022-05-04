ab6a96612672afb92097d2b6a9da496ea2516082
Blerand Ismaili <94268979+BlerandIsm@users.noreply.github.com>
2022-05-03 21:14:29 -0400

    update js

---
+++ public/assets/js/db.js
@@ -0,0 +1,52 @@
+const request = window.indexedDB.open("budget-tracker", 1);
+let db;
+d
+request.onupgradeneeded = (event) => {
+  db = event.target.result; 
+  db.createObjectStore("Budget", { autoIncrement: true });
+};
+
+request.onsuccess = (event) => {
+  
+  db = event.target.result;
+  if (navigator.onLine) {
+    uploadBudgetInfo();
+  }
+};
+
+function saveRecord(data) {
+  console.log(data);
+  const transaction = db.transaction(["Budget"], "readwrite");
+  const budgetStore = transaction.objectStore("Budget"); 
+  budgetStore.add(data); 
+}
+
+async function uploadBudgetInfo() {
+  const transaction = db.transaction(["Budget"], "readwrite");
+  const budgetStore = transaction.objectStore("Budget");
+  const budgetInfo = budgetStore.getAll();
+
+  budgetInfo.onsuccess = () => {
+    if (budgetInfo.result.length > 0) {
+      fetch("api/transaction/bulk", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(budgetInfo.result),
+      })
+        .then((result) => result.json())
+        .then((response) => {
+          if (response.message) {
+            throw new Error(message);
+          }
+          const transaction = db.transaction(["Budget"], "readwrite");
+          const budgetStore = transaction.objectStore("Budget");
+          budgetStore.clear();
+
+          alert("Your budget info has been synced!");
+          window.location.reload();
+        });
+    }
+  };
+}
+
+window.addEventListener('online', uploadBudgetInfo);
